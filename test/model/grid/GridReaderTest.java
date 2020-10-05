edd9459552c5e149224e5a54f30d56f6be9e0804
Yasser <ye9@duke.edu>
2020-10-05 16:45:27 -0400

    Removed unnecessary imports

--- test/model/grid/GridReaderTest.java
+++ test/model/grid/GridReaderTest.java
@@ -2,7 +2,6 @@ package model.grid;
 
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 import java.util.ResourceBundle;
@@ -14,9 +13,11 @@ public class GridReaderTest {
   public void testGridRead_simpleGrid() {
     ResourceBundle resources = ResourceBundle.getBundle("resources.data");
     GridReader gridReader = new GridReader();
+
     List<String[]> expectedOutput = List.of(new String[]{"3","3"}, new String[]{"1","0","1"},
         new String[]{"0", "1", "1"}, new String[]{"1", "1", "1"});
     List<String[]> actualOutput = gridReader.readAll(GridReader.class.getClassLoader().getResourceAsStream(resources.getString("DataSource")));
+
     for (int row = 0; row < expectedOutput.size(); row++) {
       assertArrayEquals(expectedOutput.get(row), actualOutput.get(row));
     }
