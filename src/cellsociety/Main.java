c3b3c787d3e37c90f300d3592f30b3e975b972b7
Fernanda Corona <fec6@duke.edu>
2020-10-05 19:22:33 -0700

    added visualization

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,8 +1,14 @@
 package cellsociety;
 
-import java.util.Scanner;
+
+import java.util.concurrent.TimeUnit;
+
 import model.grid.Grid;
+import view.GUI;
+import view.GameSimulator;
 import view.GridView;
+
+import javax.swing.*;
 
 /**
  * Gets data from a given file and prints the current state of the cells.
@@ -16,22 +22,27 @@ public class Main {
     /**
      * Start of the program.
      */
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         String filename = "data/simpleGrid";
         Grid grid = new Grid(filename);
         GridView view = new GridView();
         view.displayGrid(grid);
-
-        Scanner myObj = new Scanner(System.in);
+        JPanel mySimulator = new GameSimulator(grid, 500, 500);
+        GUI userinterface = new GUI(grid, mySimulator);
         while (true) {
-            System.out.println("Press 'N' to compute next step, 'Q' to quit");
-            String key = myObj.nextLine();
-            if (key.equals("N")) {
+            if(userinterface.shouldcontinue()) {
                 grid.performNextStep();
-                view.displayGrid(grid);
+                mySimulator = new GameSimulator(grid, 500, 500);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-            if (key.equals("Q")) {
-                System.exit(0);
+            if(userinterface.wantnewFile()) {
+                grid = new Grid(userinterface.chooseNewFile());
+                mySimulator = new GameSimulator(grid, 500, 500);
+                userinterface.resetGUI(grid, mySimulator);
             }
         }
     }
