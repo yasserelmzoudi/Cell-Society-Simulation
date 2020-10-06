01d48d974372cb2b09dd919df9f8984325852354
Fernanda Corona Rizo <fec6@duke.edu>
2020-10-05 21:56:18 -0700

    interface

--- src/cellsociety/Main.java
+++ src/cellsociety/Main.java
@@ -1,10 +1,16 @@
 package cellsociety;
 
-import java.util.ResourceBundle;
-import java.util.Scanner;
+
+import java.util.concurrent.TimeUnit;
+
 import model.grid.Grid;
+import view.GUI;
+import view.GameSimulator;
+import java.util.ResourceBundle;
 import model.grid.GridReader;
 import view.GridView;
+
+import javax.swing.*;
 
 /**
  * Gets data from a given file and prints the current state of the cells.
@@ -15,29 +21,34 @@ import view.GridView;
  * <p> When user presses 'Q', program quits. </p>
  */
 public class Main {
-
-  /**
-   * Start of the program.
-   */
-  public static void main(String[] args) {
-    ResourceBundle resources = ResourceBundle.getBundle("resources.data");
-    GridReader gridReader = new GridReader(
-        GridReader.class.getClassLoader().getResourceAsStream(resources.getString("DataSource")));
-    Grid grid = new Grid(gridReader);
-
-    GridView view = new GridView();
-    view.displayGrid(grid);
-
-    Scanner myObj = new Scanner(System.in);
-    while (true) {
-      System.out.println("Press 'N' to compute next step, 'Q' to quit");
-      String key = myObj.nextLine();
-      if (key.equals("N")) {
-        grid.performNextStep();
+    /**
+     * Start of the program.
+     */
+    public static void main(String[] args) {
+        ResourceBundle resources = ResourceBundle.getBundle("resources.data");
+        GridReader gridReader = new GridReader(
+                GridReader.class.getClassLoader().getResourceAsStream(resources.getString("DataSource")));
+        Grid grid = new Grid(gridReader);
+        GridView view = new GridView();
         view.displayGrid(grid);
-      }
-      if (key.equals("Q")) {
-        System.exit(0);
-      }
+        JPanel mySimulator = new GameSimulator(grid, 500, 500);
+        GUI userinterface = new GUI(grid, mySimulator);
+        while (true) {
+            if(userinterface.shouldcontinue()) {
+                grid.performNextStep();
+                mySimulator = new GameSimulator(grid, 500, 500);
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            if(userinterface.wantnewFile()) {
+                //grid = new Grid(userinterface.chooseNewFile()); not working
+                mySimulator = new GameSimulator(grid, 500, 500);
+                userinterface.resetGUI(grid, mySimulator);
+            }
+        }
+
     }
-  }
+}
