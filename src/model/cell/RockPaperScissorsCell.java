c98412fe946afc16a045874d4ea4e7dd20cf6f8e
Umika Paul <usp3@duke.edu>
2020-10-11 05:20:03 -0400

    Added modifications to Rock Paper Scissors Cell

--- src/model/cell/RockPaperScissorsCell.java
+++ src/model/cell/RockPaperScissorsCell.java
@@ -1,20 +1,46 @@
 package model.cell;
 
 import java.util.List;
+/**
+ * Subclass encapsulating logic to update cells in the Rock Paper Scissors simulation.
+ * Checks to see how many neighbors are of a state that can beat the current state and if the
+ * number is greater than <code>Threshold</code>, it changes the current cell type using an enum
+ * state accordingly.
+ */
 
 public class RockPaperScissorsCell extends Cell {
   private int state;
   private static final int THRESHOLD = 4;
 
+  /**
+   * Constructor for this class.
+   *
+   * @param row Row of cell.
+   * @param column Column of cell.
+   * @param state State cell is in.
+   */
   public RockPaperScissorsCell(int row, int column, int state) {
     super(row, column, state);
     this.state = state;
   }
 
+  /**
+   * Copy constructor.
+   *
+   * @param copyCell Makes a copy of the cell to preserve original values.
+   */
   public RockPaperScissorsCell(Cell copyCell) {
     super(copyCell);
   }
 
+  /**
+   * Updates cell based on the state of neighboring cells.
+   *
+   * @param neighbors List of neighboring cells
+   * @param newNeighbors List of neighboring cells that may have been updated.
+   * @param isUpdated Whether the cell has already been updated or not. If it has been updated,
+   *                  skip over the cell.
+   */
   @Override
   public void update(List<Cell> neighbors, List<Cell> newNeighbors, boolean[][] isUpdated) {
     int rock = 0;
@@ -33,6 +59,15 @@ public class RockPaperScissorsCell extends Cell {
 
   }
 
+  /**
+   * Based on the number of rock, paper, or scissors surrounding the cell, if the number of objects
+   * that can beat the current cell is greater than the <code>THRESHOLD</code>, then the current
+   * cell is replaced by this object.
+   *
+   * @param rock The number of surrounding rocks.
+   * @param paper The number of surrounding rocks.
+   * @param scissors The number of surrounding rocks.
+   */
   private void changeCellType(int rock, int paper, int scissors) {
     if ((getState().name().equals("ROCK")) && (paper > THRESHOLD)) {
       setCellType(CellType.PAPER);
