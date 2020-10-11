f10f13ac7b0981408701155ad8821b8748ac01a3
Umika Paul <usp3@duke.edu>
2020-10-11 05:54:09 -0400

    Modified abstract class for predator prey cell

--- src/model/cell/PPCellFeatures.java
+++ src/model/cell/PPCellFeatures.java
@@ -1,16 +1,39 @@
 package model.cell;
 
+/**
+ * Abstract class encapsulating logic for Predator Prey simulation.
+ *
+ * <p>Reproduce represents number of cycles a fish or shark can live before reproducing, and
+ * being reset back to 0.</p>
+ *
+ * <p>Energy represents the units of energy a shark has left. Fish always have energy 0.</p>
+ */
+
 public abstract class PPCellFeatures extends Cell {
 
   private int reproduce;
   private int energy;
 
+  /**
+   * Additional features of Predator Prey Cell.
+   *
+   * @param row Row in grid.
+   * @param column Column in grid.
+   * @param state State of cell.
+   * @param reproduce Number of cycles lived before reset to 0 after reproducing.
+   * @param energy Units of energy a shark has left.
+   */
   public PPCellFeatures(int row, int column, int state, int reproduce, int energy) {
     super(row, column, state);
     this.reproduce = reproduce;
     this.energy = energy;
   }
 
+  /**
+   * Copy constructor.
+   *
+   * @param copyCell Copy of cell.
+   */
   public PPCellFeatures(Cell copyCell) {
     super(copyCell);
   }
