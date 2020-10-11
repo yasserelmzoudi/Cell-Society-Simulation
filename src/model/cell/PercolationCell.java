ab4451994247cce96b814d5c51c29d7fd12006b0
Umika Paul <usp3@duke.edu>
2020-10-11 05:16:16 -0400

    Added modifications to Percolation  Cell

--- src/model/cell/PercolationCell.java
+++ src/model/cell/PercolationCell.java
@@ -1,20 +1,45 @@
 package model.cell;
 
 import java.util.List;
+/**
+ * Subclass encapsulating logic to update cells in the Percolation simulation.
+ * Checks to see if any neighboring cells are of the state <code>FULL_OPEN</code> and changes the
+ * current cell type to <code>FULL_OPEN</code> if the current cell type is <code>EMPTY_OPEN</code>
+ * using an enum state accordingly.
+ */
 
 public class PercolationCell extends Cell {
   private int state;
 
+  /**
+   * Constructor for this class.
+   *
+   * @param row Row of cell.
+   * @param column Column of cell.
+   * @param state State cell is in.
+   */
   public PercolationCell(int row, int column, int state) {
     super(row, column, state);
     this.state = state;
   }
 
+  /**
+   * Copy constructor.
+   *
+   * @param copyCell Makes a copy of the cell to preserve original values.
+   */
   public PercolationCell(Cell copyCell) {
     super(copyCell);
   }
 
-
+  /**
+   * Updates cell based on the state of neighboring cells.
+   *
+   * @param neighbors List of neighboring cells
+   * @param newNeighbors List of neighboring cells that may have been updated.
+   * @param isUpdated Whether the cell has already been updated or not. If it has been updated,
+   *                  skip over the cell.
+   */
   @Override
   public void update(List<Cell> neighbors, List<Cell> newNeighbors, boolean[][] isUpdated) {
     boolean changeState = false;
