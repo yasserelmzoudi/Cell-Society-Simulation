911a74b2d320fe13172e228cb3fe0f1ce18200c6
Fernanda Corona <fec6@duke.edu>
2020-10-12 21:07:38 -0700

    Revert "Merged conflicts"
    This reverts commit cb4c3d2abdd3f12c5d8349f7150ccb1ec5f81845.

--- src/model/grid/GridType.java
+++ src/model/grid/GridType.java
@@ -1,14 +1,10 @@
 package model.grid;
 
-/**
- * Type of Grid.
- */
-
 public enum GridType {
   GAME_OF_LIFE,
   PERCOLATION,
-  ROCK_PAPER_SCISSORS,
-  SPREADING_OF_FIRE,
+  RPS,
+  FIRE_SPREAD,
   SEGREGATION,
   PREDATOR_PREY
 }
