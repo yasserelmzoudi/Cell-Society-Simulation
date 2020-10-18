29f102e3178ca909fef18bb1e2d2e2a727e9e8f4
Umika Paul <usp3@duke.edu>
2020-10-18 16:53:27 -0400

    Fixed triangle game pane

--- src/view/GamePaneShapes/TriangleGamePane.java
+++ src/view/GamePaneShapes/TriangleGamePane.java
@@ -4,29 +4,36 @@ import javafx.scene.shape.Shape;
 import model.grid.Grid;
 import view.Shapes.Triangle;
 
-
+/**
+ * Class that makes the triangle game pane.
+ */
 public class TriangleGamePane extends GamePane {
+
+    private static final double BASE_WIDTH = 1.5;
     private Shape[][] allShapes;
 
+    /**
+     * Constructor for this class.
+     *
+     * @param grid Triangle grid.
+     * @param width Width of grid.
+     * @param height Height of grid.
+     */
     public TriangleGamePane(Grid grid, int width, int height){
         super(grid, width, height);
     }
 
-    private double triangleBase() {
-        return cellWidth()/1.5;
-    }
-
-    private double traingleHeight() {
-        return cellHeight();
-    }
-
-
+    /**
+     * Makes the array of triangles.
+     *
+     * @param myGrid The given grid.
+     */
     @Override
     public void makeArray(Grid myGrid) {
         allShapes = new Triangle[myGrid.gridRows()][myGrid.gridColumns()];
         for (int r = 0; r < myGrid.gridRows(); r++) {
             for (int c = 0; c < myGrid.gridColumns(); c++) {
-                Triangle mynewPixel = new Triangle(r, c, triangleBase(), traingleHeight());
+                Triangle mynewPixel = new Triangle(r, c, triangleBase(), triangleHeight());
                 allShapes[r][c] = mynewPixel;
             }
         }
@@ -35,6 +42,14 @@ public class TriangleGamePane extends GamePane {
     @Override
     public Shape[][] getInitialArray() {
         return allShapes;
+    }
+
+    private double triangleBase() {
+        return cellWidth()/BASE_WIDTH;
+    }
+
+    private double triangleHeight() {
+        return cellHeight();
     }
 
 }
