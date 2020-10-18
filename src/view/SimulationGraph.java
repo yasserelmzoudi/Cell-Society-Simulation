f794df95f36c1e18e9612dfcb824a3d39d6579c4
Yasser <ye9@duke.edu>
2020-10-18 00:35:07 -0400

    Added labels to graph from resource

--- src/view/SimulationGraph.java
+++ src/view/SimulationGraph.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
@@ -21,6 +22,9 @@ public class SimulationGraph {
 
   private static final int GRAPH_WINDOW_WIDTH = 400;
   private static final int GRAPH_WINDOW_HEIGHT = 400;
+  private static final String GRAPH_RESOURCE = "resources.graphLabels";
+
+  private  ResourceBundle graphLabelSource;
 
   private Grid grid;
   private NumberAxis xAxis;
@@ -35,6 +39,7 @@ public class SimulationGraph {
   public SimulationGraph(Grid grid, String simulationType) {
     this.grid = grid;
     this.simulationType = simulationType;
+    graphLabelSource = ResourceBundle.getBundle(GRAPH_RESOURCE);
 
     BorderPane graphRoot = new BorderPane();
     Scene scene = new Scene(graphRoot, GRAPH_WINDOW_WIDTH, GRAPH_WINDOW_HEIGHT);
@@ -59,8 +64,8 @@ public class SimulationGraph {
   public LineChart createSimulationGraph() {
     LineChart<Number, Number> simulationGraph = new LineChart<>(xAxis, yAxis);
     simulationGraph.setCreateSymbols(false);
-    xAxis.setLabel("yAxis");
-    yAxis.setLabel("xAxis");
+    xAxis.setLabel(graphLabelSource.getString("xAxis"));
+    yAxis.setLabel(graphLabelSource.getString("yAxis"));
     simulationGraph.setTitle(simulationType);
 
     for (String cellTypeName: grid.getAllTypes()) {
