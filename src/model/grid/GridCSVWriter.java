6cc6b34c28dfcfd52bffb28a9ba3b77f9b9ed5a0
Umika Paul <usp3@duke.edu>
2020-10-18 18:06:58 -0400

    refactored csv writer and added comments

--- src/model/grid/GridCSVWriter.java
+++ src/model/grid/GridCSVWriter.java
@@ -26,6 +26,7 @@ public class GridCSVWriter {
   private int gridWidth;
   private ResourceBundle errorMessageSource;
   private static final String EXCEPTION_RESOURCE = "resources.exceptionMessages";
+  private static final char FILE_DELIMITER = ',';
 
 
   /**
@@ -59,6 +60,9 @@ public class GridCSVWriter {
     }
   }
 
+  /**
+   * Set up for saving a properties file.
+   */
   private void setUpSaveFile() {
     try (OutputStream saveFile = new FileOutputStream("src/resources/" + title + ".properties")) {
       Properties prop = new Properties();
@@ -71,24 +75,24 @@ public class GridCSVWriter {
     }
   }
 
+  /**
+   * Saves a csv file. Writes the grid height and grid width to the file, and the states of all
+   * the cells.
+   */
   public void saveFile() {
     checkIfTitleProvided();
     try{
       Writer saveFile = new FileWriter("data/" + title + ".csv");
-      CSVWriter writer = new CSVWriter(saveFile, ',', CSVWriter.NO_QUOTE_CHARACTER,
+      CSVWriter writer = new CSVWriter(saveFile, FILE_DELIMITER, CSVWriter.NO_QUOTE_CHARACTER,
           CSVWriter.DEFAULT_ESCAPE_CHARACTER,
           CSVWriter.DEFAULT_LINE_END);
       List<String[]> data = new ArrayList<>();
-      String[] rowData = {String.valueOf(gridHeight), String.valueOf(gridWidth)};
-      data.add(rowData);
 
-      for (int row = 0; row < gridHeight; row++) {
-        rowData = new String[gridWidth];
-        for (int column = 0; column < gridWidth; column++) {
-          rowData[column] = String.valueOf(grid.getCellTypeState(row, column));
-        }
-        data.add(rowData);
-      }
+      String[] gridWidthHeight = {String.valueOf(gridHeight), String.valueOf(gridWidth)};
+      data.add(gridWidthHeight);
+
+      addRows(data);
+
       writer.writeAll(data);
       writer.close();
     } catch (IOException e) {
@@ -96,7 +100,17 @@ public class GridCSVWriter {
     }
   }
 
-  public void writeFile(Grid cells) {
+  private void addRows(List<String[]> data) {
+    String[] rowData;
+    for (int row = 0; row < gridHeight; row++) {
+      rowData = new String[gridWidth];
+      for (int column = 0; column < gridWidth; column++) {
+        rowData[column] = String.valueOf(grid.getCellTypeState(row, column));
+      }
+      data.add(rowData);
+    }
+  }
 
+  public void writeFile(Grid cells) {
   }
 }
