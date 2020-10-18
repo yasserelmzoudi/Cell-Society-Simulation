767314238f128f66fe3a4a89f1245fc0341f41d1
Umika Paul <usp3@duke.edu>
2020-10-18 16:46:11 -0400

    Changed hexagon class

--- src/view/Shapes/Hexagon.java
+++ src/view/Shapes/Hexagon.java
@@ -3,36 +3,69 @@ package view.Shapes;
 import javafx.scene.shape.Polygon;
 import model.grid.Grid;
 
-public class Hexagon extends Polygon{
-    private int myRow;
-    private int myColumn;
-    private Double[] pointArray;
-    public Hexagon (int row, int column, double hexWidth, double hexLength) {
-        myRow = row;
-        myColumn =column;
-        makeInitialShape(myRow, myColumn, hexWidth, hexLength);
+/**
+ * Class that makes the hexagon shape.
+ */
+public class Hexagon extends Polygon {
 
+  private static final double HALF_UNIT = 0.5;
+  private static final double UNIT = 1;
+  private int myRow;
+  private int myColumn;
+  private Double[] pointArray;
+
+
+    /**
+     * Constructor for this class.
+     *
+     * @param row Row of hexagon.
+     * @param column Column of hexagon.
+     * @param hexWidth Width of hexagon.
+     * @param hexLength Height of hexagon.
+     */
+  public Hexagon(int row, int column, double hexWidth, double hexLength) {
+    myRow = row;
+    myColumn = column;
+    makeInitialShape(myRow, myColumn, hexWidth, hexLength);
+  }
+
+    /**
+     * Makes the hexagon shape.
+     *
+     * @param row Row of hexagon.
+     * @param col Column of hexagon.
+     * @param hexWidth Width of hexagon.
+     * @param hexLength Height of hexagon.
+     */
+  public void makeInitialShape(double row, double col, double hexWidth, double hexLength) {
+    if (isOdd(row)) {
+      col += HALF_UNIT;
+      row += row / 2;
+    } else if (isNotFirstRow(row) && isEven(row)) {
+      row += row / 2;
     }
+    row += HALF_UNIT;
+    pointArray = new Double[]{
+        col * hexWidth, row * hexLength,
+        (col + HALF_UNIT) * hexWidth, (row - HALF_UNIT) * hexLength,
+        (col + UNIT) * hexWidth, row * hexLength,
+        (col + UNIT) * hexWidth, (row + UNIT) * hexLength,
+        (col + HALF_UNIT) * hexWidth, (row + HALF_UNIT + UNIT) * hexLength,
+        col * hexWidth, (row + UNIT) * hexLength
+    };
+    this.getPoints().addAll(pointArray);
+  }
 
-    public void makeInitialShape(double row, double col, double hexWidth, double hexLength) {
-        if (row % 2 == 1) {
-            col = col + 0.5;
-            row = row + row / 2;
-        } else if (row != 0 && row % 2 == 0) {
-            row = row + row / 2;
-        }
-        row = row + 0.5;
-        pointArray = new Double[]{
-                 col * hexWidth, row * hexLength,
-                (col + 0.5) * hexWidth, (row - 0.5) * hexLength,
-                (col + 1) * hexWidth,  row * hexLength,
-                (col + 1) * hexWidth,  (row + 1) * hexLength,
-                 (col + 0.5) * hexWidth,  (row + 1.5) * hexLength,
-                col * hexWidth,  (row + 1) * hexLength
-        };
-        this.getPoints().addAll(pointArray);
-    }
+  private boolean isEven(double coordinate) {
+    return coordinate % 2 == 0;
+  }
 
+  private boolean isOdd(double coordinate) {
+    return coordinate % 2 == 1;
+  }
 
+  private boolean isNotFirstRow(double coordinate) {
+    return coordinate != 0;
+  }
 
 }
