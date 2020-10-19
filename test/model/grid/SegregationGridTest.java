9226eb30250426818aec14d9052c099c9da776cb
Umika Paul <usp3@duke.edu>
2020-10-18 20:02:42 -0400

    added tests for spreading of fire grid

---
+++ test/model/grid/SegregationGridTest.java
@@ -0,0 +1,46 @@
+package model.grid;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ResourceBundle;
+import model.cell.Cell;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SegregationGridTest {
+  private SegregationGrid grid;
+  ResourceBundle resources = ResourceBundle.getBundle("resources.data");
+  InputStream data = Grid.class.getClassLoader()
+      .getResourceAsStream(resources.getString("SegregationGridTest"));
+
+  @BeforeEach
+  public void setup() {
+    grid = new SegregationGrid(data, "Finite", "Complete");
+  }
+
+  @Test
+  public void checkGridHeight() {
+    assertEquals(30, grid.gridHeight);
+  }
+
+  @Test
+  public void checkGridWidth() {
+    assertEquals(30, grid.gridHeight);
+  }
+
+  @Test
+  public void getCellTypes() {
+    List<String> expectedCellTypes = Arrays.asList("X","O","NO_RACE");
+    List<String> actualCellTypes = grid.getAllTypes();
+    assertTrue(expectedCellTypes.size() == actualCellTypes.size() &&
+        expectedCellTypes.containsAll(actualCellTypes) &&
+        expectedCellTypes.containsAll(actualCellTypes));
+  }
+
+
+
+}
